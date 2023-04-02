b3dafe1e2abb30438a23e52b0a8a6deb3a517c87
Anatoliy Yakovenko <yakovenko.anatoliy@gmail.com>
2023-04-02 14:27:29 +0300

    complete task08

--- index.js
+++ index.js
@@ -1,15 +1,25 @@
 const refs = {
   image: document.querySelector("#image"),
-  aFlag: document.querySelector(".flag"),
+  aFlag: document.querySelector("#a"),
+  bFlag: document.querySelector("#b"),
+  cFlag: document.querySelector("#c"),
+  dFlag: document.querySelector("#d"),
 };
 
 const handleClick = (event) => {
   event.target.classList.toggle("photo");
-  //   if (!event.target.classList.contains("photo")) {
-  //     event.target.classList.add("");
-  //   } else {
-  //     event.target.classList.remove("reverse");
-  //   }
 };
-
 refs.image.addEventListener("click", handleClick);
+
+const handleKeyDown = (event) => {
+  if (event.key === "a" || event.key === "A") {
+    refs.aFlag.scrollIntoView();
+  } else if (event.key === "b" || event.key === "B") {
+    refs.bFlag.scrollIntoView();
+  } else if (event.key === "c" || event.key === "C") {
+    refs.cFlag.scrollIntoView();
+  } else if (event.key === "d" || event.key === "D") {
+    refs.dFlag.scrollIntoView();
+  } else return;
+};
+document.addEventListener("keydown", handleKeyDown);
